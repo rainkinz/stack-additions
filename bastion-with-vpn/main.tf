f469d4c2e45f576de09101c7ea2dea288d54154d
rainkinz <brendan.grainger@gmail.com>
2016-09-21 10:20:46 -0400

    rename subnet_ip to public_subnet_id to better descript what its supposed to be

--- bastion-with-vpn/main.tf
+++ bastion-with-vpn/main.tf
@@ -1,6 +1,9 @@
 /**
  * The bastion host acts as the "jump point" for the rest of the infrastructure.
- * Since most of our instances aren't exposed to the external internet, the bastion acts as the gatekeeper for any direct SSH access.
+ * Since most of our instances aren't exposed to the external internet, the 
+ * bastion acts as the gatekeeper for any direct SSH access and allows clients
+ * to connect via vpn.
+ * 
  * The bastion is provisioned using the key name that you pass to the stack (and hopefully have stored somewhere).
  * If you ever need to access an instance directly, you can do it by "jumping through" the bastion.
  *
@@ -10,12 +13,12 @@
  * Usage:
  *
  *    module "bastion" {
- *      source            = "github.com/segmentio/stack/bastion"
- *      region            = "us-west-2"
+ *      source            = "github.com/rainkinz/stack-additions/bastion-with-vpn"
+ *      region            = "us-east-1
  *      security_groups   = "sg-1,sg-2"
  *      vpc_id            = "vpc-12"
  *      key_name          = "ssh-key"
- *      subnet_id         = "pub-1"
+ *      public_subnet_id         = "pub-1"
  *      environment       = "prod"
  *    }
  *
@@ -38,11 +41,18 @@ variable "vpc_id" {
   description = "VPC ID"
 }
 
+variable "vpc_cidr" {
+}
+
 variable "key_name" {
   description = "The SSH key pair, key name"
 }
 
-variable "subnet_id" {
+variable "private_key_path" {
+  description = "Path to the private key part of the SSH key pair."
+}
+
+variable "public_subnet_id" {
   description = "A external subnet id"
 }
 
@@ -50,27 +60,66 @@ variable "environment" {
   description = "Environment tag, e.g prod"
 }
 
-module "ami" {
-  source        = "github.com/terraform-community-modules/tf_aws_ubuntu_ami/ebs"
-  region        = "${var.region}"
-  distribution  = "trusty"
-  instance_type = "${var.instance_type}"
+
+/* module "ami" { */
+/*   source        = "github.com/terraform-community-modules/tf_aws_ubuntu_ami/ebs" */
+/*   region        = "${var.region}" */
+/*   distribution  = "trusty" */
+/*   instance_type = "${var.instance_type}" */
+/* } */
+
+
+variable "nat-amis" {
+  description = "NAT AMIs to launch the instances with"
+  default = {
+    us-west-1 = "ami-049d8641"
+    us-east-1 = "ami-311a1a5b"
+  }
 }
 
+
 resource "aws_instance" "bastion" {
-  ami                    = "${module.ami.ami_id}"
+  ami                    = "${lookup(var.nat-amis, var.region)}"
   source_dest_check      = false
   instance_type          = "${var.instance_type}"
-  subnet_id              = "${var.subnet_id}"
+
+  subnet_id              = "${var.public_subnet_id}"
   key_name               = "${var.key_name}"
   vpc_security_group_ids = ["${split(",",var.security_groups)}"]
   monitoring             = true
-  user_data              = "${file(format("%s/user_data.sh", path.module))}"
+
+  // Example of how we might deploy some userdata
+  // user_data              = "${file(format("%s/user_data.sh", path.module))}"
+  /* In order to route traffic, they need to have the ’source destination check' parameter disabled */
+  source_dest_check = false
 
   tags {
     Name        = "bastion"
     Environment = "${var.environment}"
   }
+
+  connection {
+    // Note the user is EC2 user
+    user = "ec2-user"
+    key_file = "${var.private_key_path}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      /* Since we're using AWS NAT no need to do this */
+      /* "sudo iptables -t nat -A POSTROUTING -j MASQUERADE", */
+      /* "echo 1 > /proc/sys/net/ipv4/conf/all/forwarding", */
+      /* Install docker */ 
+      "curl -sSL https://get.docker.com/ | sudo sh",
+      "sudo service docker start",
+      /* Initialize open vpn data container */
+      "sudo mkdir -p /etc/openvpn",
+      "sudo docker run --name ovpn-data -v /etc/openvpn busybox",
+      /* Generate OpenVPN server config */
+      "sudo docker run --volumes-from ovpn-data --rm gosuri/openvpn ovpn_genconfig -p ${var.vpc_cidr} -u udp://${aws_instance.bastion.public_ip}"
+    ]
+  }
+
 }
 
 resource "aws_eip" "bastion" {
--- bastion-with-vpn/user_data.sh
+++ bastion-with-vpn/user_data.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# NOTE: This is just an example of how one might create a file `foo` on
+# the remote host.
 
 set -e
 cat <<EOF > foo
